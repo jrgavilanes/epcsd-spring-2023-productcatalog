84f1c02eeba957f20ffe50318e343e507d67c7a2
Juan R. Gavilanes <jrgavilanes@gmail.com>
2023-05-07 20:23:56 +0200

    add createProduct

--- src/main/java/edu/uoc/epcsd/productcatalog/controllers/ProductController.java
+++ src/main/java/edu/uoc/epcsd/productcatalog/controllers/ProductController.java
@@ -2,17 +2,21 @@ package edu.uoc.epcsd.productcatalog.controllers;
 
 import edu.uoc.epcsd.productcatalog.entities.Product;
 import edu.uoc.epcsd.productcatalog.kafka.ProductMessage;
+import edu.uoc.epcsd.productcatalog.repositories.CategoryRepository;
 import edu.uoc.epcsd.productcatalog.repositories.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @RestController
@@ -23,15 +27,37 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private KafkaTemplate<String, ProductMessage> productKafkaTemplate;
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public List<Product> getAllProducts() {
         log.trace("getAllProducts");
-
         return productRepository.findAll();
     }
 
     // add the code for the missing operations here
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Map<String, Object>> createProduct(@RequestBody @Valid Product product,
+                                                             @RequestParam(name = "category_id", required = false) Long categoryId) {
+        log.trace("createProduct");
+        if (categoryId != null) {
+            var category = categoryRepository.findById(categoryId);
+            if (category.isPresent()) {
+                product.setCategory(category.get());
+            } else {
+                Map<String, Object> body = new LinkedHashMap<>();
+                body.put("timestamp", LocalDateTime.now());
+                body.put("message", "ID Category not found: " + categoryId);
+
+                return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+            }
+        }
+        productRepository.save(product);
+        return null;
+    }
 }
